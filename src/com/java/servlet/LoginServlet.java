a5b828862638984369e403217ca0b7eeb715d47d
abhijeet-gi <abhijit.hole16@gmail.com>
2020-12-07 19:37:07 +0530

    version 1

---
+++ src/com/java/servlet/LoginServlet.java
@@ -0,0 +1,22 @@
+package com.java.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet ("/login")
+
+public class LoginServlet extends HttpServlet{
+	//private LoginService lservice;
+	public void init() {
+		
+	}
+	
+	public void doPost(HttpServletRequest req,HttpServletResponse res) {
+		
+		
+		
+		
+	}
+}
